cd6f0a8758d20c2e4515d9e069daf2a2bc2f4897
Drummond <drummond05@proton.me>
2026-03-21 19:48:03 -0300

    feat: implement feature to change title and content before create a new note

---
+++ backend/src/test/java/com/notesapi/notes_api/helpers/TestNoteHelper.java
@@ -0,0 +1,33 @@
+package com.notesapi.notes_api.helpers;
+
+import com.notesapi.notes_api.note.NoteRepository;
+import com.notesapi.notes_api.note.entities.Note;
+import com.notesapi.notes_api.user.entities.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TestNoteHelper {
+
+
+    private final NoteRepository noteRepository;
+
+    private String title = "New note";
+    private String content = "new content";
+
+    public TestNoteHelper title(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public TestNoteHelper content(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public Note createNote(User user) {
+        Note note = Note.builder().title(title).content(content).user(user).build();
+        return noteRepository.save(note);
+    }
+}
